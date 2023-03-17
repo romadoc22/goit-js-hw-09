44eae1fb0f474ee9a8328ac08f3d14a3bb6d9a63
romadoc22 <113574669+romadoc22@users.noreply.github.com>
2023-03-17 22:00:30 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,8 +6,8 @@
 //     // Reject
 //   }
 // }
-import Notiflix from 'notiflix';
 
+import Notiflix from 'notiflix';
 const form = document.querySelector('.form');
 
 function createPromise(position, delay) {
@@ -25,17 +25,21 @@ function createPromise(position, delay) {
 
 form.addEventListener('submit', event => {
   event.preventDefault();
-  const delay = Number(event.target.elements.delay.value);
+  let delay = Number(event.target.elements.delay.value);
   const step = Number(event.target.elements.step.value);
   const amount = Number(event.target.elements.amount.value);
   let position = 1;
   for (let i = 0; i < amount; i++) {
     createPromise(position, delay)
       .then(({ position, delay }) => {
-        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
       })
       .catch(({ position, delay }) => {
-        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
       });
     delay += step;
     position++;
